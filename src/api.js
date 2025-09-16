57fd16e19f16fc6e97754a1c689acc234266e8b3
viviana <lilith37.vl@gmail.com>
2025-09-16 16:38:05 -0300

    carpeta api.js

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export function getProducts() {
+  return fetch('https://fakestoreapi.com/products')
+    .then(response => response.json()) // acá obtenés el JSON
+    .then(data => {
+      console.log(data); // si querés ver qué llega
+      return data;       // devolvés los productos al .then() de index.js
+    });
+}
