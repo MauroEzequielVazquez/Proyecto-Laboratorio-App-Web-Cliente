69ea496c67bf28222e0f0eef32600a499c2c34f5
viviana <lilith37.vl@gmail.com>
2025-09-17 09:39:29 -0300

    cards.js add button y evento click

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,25 +1,50 @@
-import { getProducts } from "../api.js"
 
-export function RenderCards(){
-let productList = document.querySelector("#product-list");
-console.log(productList);
-getProducts().then((products) => {
-  let tempalte = "";
-  products.forEach((p) => {
-    tempalte += `
-      <div class="col">
-        <div class="card">
-          <img src="${p.image}" class="card-img-top" alt="${p.title}">
-          <div class="card-body">
-            <h5 class="card-title">${p.title}</h5>
-            <p class="card-text">${p.description}</p>
-          </div>
-        </div>
-      </div>
-    `;
-  });
 
-  productList.innerHTML = tempalte;
-});
+import { getProducts } from "../api.js"; 
 
-}
+import { Modal } from "./modal.js"; 
+
+export function RenderCards() { 
+
+  let productList = document.querySelector("#product-list"); 
+
+  console.log(productList); 
+
+  getProducts().then((products) => { 
+    let template = ""; 
+
+    products.forEach((p) => { 
+      template += ` 
+        <div class="col"> 
+          <div class="card justify-content-center align-items-center" style="width: 300px;"> 
+            <img src="${p.image}" class="card-img-top" alt="${p.title}" style="height: 300px; width: 259px; object-fit: contain;"> 
+            <div class="card-body" style="width: 300px; overflow: hidden;"> 
+              <h5 class="card-title text-truncate" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"> 
+              ${p.title} 
+              </h5> 
+            </div> 
+            <div class="mb-3"> 
+              <button class="btn btn-dark" id="btn-${p.id}">Más detalles</button> 
+            </div> 
+          </div> 
+        </div> 
+        `; 
+    }); 
+
+    productList.innerHTML = template; 
+
+    products.forEach((p) => { 
+      let button = document.querySelector(`#btn-${p.id}`); 
+
+      if (button) { 
+        button.addEventListener('click', () => { 
+        // console.log(`Botón de producto ${p.id} clickeado`); 
+        Modal(p); 
+        }); 
+      } 
+
+    }); 
+
+  }); 
+
+} 
