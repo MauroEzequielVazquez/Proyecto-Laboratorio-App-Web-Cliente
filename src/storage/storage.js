ccfa7da555375d8f990358d3aa7d7387b5153861
viviana <lilith37.vl@gmail.com>
2025-09-18 18:53:30 -0300

    creacion local storage para cart

---
+++ src/storage/storage.js
@@ -0,0 +1,22 @@
+const STORAGE_KEY = "cart"; // para no repetir la palabra cart , creo la variable 
+
+//inicializo 
+export function initiLocalStorage() { 
+    if (!localStorage.getItem(STORAGE_KEY)) { 
+        localStorage.setItem(STORAGE_KEY, JSON.stringify([])); 
+    } 
+} 
+
+export function getFromLocalStorage(){ 
+    return JSON.parse(localStorage.getItem(STORAGE_KEY)) 
+} 
+
+export function saveToLocalStorage(item){ // guardo los datos y como estan en tipo de dato objeto, los convierto a string 
+    let cart = getFromLocalStorage(); 
+    cart.push(item); // tengo que llamar al local storage y pushear el item, guarda el objeto en la Ultima posicion 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(cart)); 
+} 
+
+export function setItemTolocalStorage(items){ // guardo los datos y como estan en tipo de dato objeto, los convierto a string 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items)); 
+}
