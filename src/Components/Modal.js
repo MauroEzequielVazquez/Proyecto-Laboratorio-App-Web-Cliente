e97df05e0aaa196d17bac0c08073ea413bd395b5
dalvezPsag <160765807+dalvezPsag@users.noreply.github.com>
2025-09-19 08:39:43 -0300

    Modificacion de asignacion de rutas de importacion

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,70 +1,82 @@
-import { getFromLocalStorage, saveToLocalStorage, setItemTolocalStorage } from "../storage/storage.js"; 
-import { contador } from "./count.js"; 
-import { addEventListeners } from "./count.js"; 
-export function Modal(prod) { 
-const container = document.querySelector("#prodModal"); 
-container.innerHTML = ` 
-<div class="modal-dialog modal-lg"> 
-<div class="modal-content"> 
-<div class="modal-header"> 
-<h1 class="modal-title fs-5" id="prodModalLabel"> 
-${prod.title} Modal title 
-</h1> 
-<button 
-type="button" 
-class="btn-close" 
-data-bs-dismiss="modal" 
-aria-label="Close"> 
-</button> 
-</div> 
-<div class="modal-body"> 
-<div class="row"> 
-<div class="col-md-6"> 
-<img 
-src="${prod.image}" 
-class="img-fluid" 
-alt="${prod.title}" 
-style="height: 300px; width: 259px; object-fit: contain;"> 
-</div> 
-<div class="col-md-6 d-flex justify-content-center align-items-center"> 
-<div class="d-flex flex-column gap-3"> 
-<p>${prod.description}</p> 
-<p style = "width: 150px;"> 
-Price: USD $${prod.price} 
-</p> 
-</div> 
-</div> 
-${contador(prod.id)} 
-</div> 
-<div class="modal-footer"> 
-<button 
-type="button" 
-class="btn btn-secondary" 
-data-bs-dismiss="modal"> 
-Cerrar 
-</button> 
-<button 
-type="button" 
-class="btn btn-dark" 
-id="addToCartBtn-${prod.id}" 
-data-id="${prod.id}"> 
-Agregar al Carrito 
-</button> 
-</div> 
-</div> 
-</div> 
-`; 
-const bootstrapModal = new bootstrap.Modal(container); 
-bootstrapModal.show(); 
-addEventListeners(prod.id, 1); 
-const btnAddToCart = document.querySelector(`#addToCartBtn-${prod.id}`); 
-btnAddToCart.addEventListener("click", () => { 
-prod.qqty = ++document.querySelector(`#Contador-${prod.id}`).textContent; 
-// valido si existe en el localStorage, mediante el metodo creado en Storage.js 
-let dataStorage = getFromLocalStorage(); 
-let filtered = dataStorage.filter(p => p.id !== prod.id); 
-filtered.push(prod); 
-setItemTolocalStorage(filtered); 
-// saveToLocalStorage(prod); 
-}) 
+import { getFromLocalStorage, saveToLocalStorage, setItemTolocalStorage } from "../storage/storage.js";
+import { contador } from "./count.js";
+import { addEventListeners } from "./count.js";
+
+export function Modal(prod) {
+    const container = document.querySelector("#prodModal");
+
+    container.innerHTML = `
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="prodModalLabel">
+                        ${prod.title} Modal title
+                    </h1>
+                    <button 
+                        type="button" 
+                        class="btn-close" 
+                        data-bs-dismiss="modal" 
+                        aria-label="Close">
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img 
+                                src="${prod.image}" 
+                                class="img-fluid" 
+                                alt="${prod.title}" 
+                                style="height: 300px; width: 259px; object-fit: contain;">
+                        </div>
+
+                        <div class="col-md-6 d-flex justify-content-center align-items-center">
+                            <div class="d-flex flex-column gap-3">
+                                <p>${prod.description}</p>
+                                <p style="width: 150px;">
+                                    Price: USD $${prod.price}
+                                </p>
+                            </div>
+                        </div>
+
+                        ${contador(prod.id)}
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button 
+                        type="button" 
+                        class="btn btn-secondary" 
+                        data-bs-dismiss="modal">
+                        Cerrar
+                    </button>
+                    <button 
+                        type="button" 
+                        class="btn btn-dark" 
+                        id="addToCartBtn-${prod.id}" 
+                        data-id="${prod.id}">
+                        Agregar al Carrito
+                    </button>
+                </div>
+            </div>
+        </div>
+    `;
+
+    const bootstrapModal = new bootstrap.Modal(container);
+    bootstrapModal.show();
+
+    addEventListeners(prod.id, 1);
+
+    const btnAddToCart = document.querySelector(`#addToCartBtn-${prod.id}`);
+    btnAddToCart.addEventListener("click", () => {
+        prod.qqty = ++document.querySelector(`#Contador-${prod.id}`).textContent;
+
+        // valido si existe en el localStorage, mediante el metodo creado en Storage.js
+        let dataStorage = getFromLocalStorage();
+        let filtered = dataStorage.filter(p => p.id !== prod.id);
+        filtered.push(prod);
+        setItemTolocalStorage(filtered);
+
+        // saveToLocalStorage(prod);
+    });
 }
--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const STORAGE_KEY = "cart"; // para no repetir la palabra cart , creo la variable
 
 export function initiLocalStorage() {  //inicializo
@@ -23,27 +22,3 @@ export function saveToLocalStorage(item){ // guardo los datos y como estan en ti
 export function setItemTolocalStorage(items){ // guardo los datos y como estan en tipo de dato objeto, los convierto a string
     localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
 }
-=======
-const STORAGE_KEY = "cart"; // para no repetir la palabra cart , creo la variable 
-
-//inicializo 
-export function initiLocalStorage() { 
-    if (!localStorage.getItem(STORAGE_KEY)) { 
-        localStorage.setItem(STORAGE_KEY, JSON.stringify([])); 
-    } 
-} 
-
-export function getFromLocalStorage(){ 
-    return JSON.parse(localStorage.getItem(STORAGE_KEY)) 
-} 
-
-export function saveToLocalStorage(item){ // guardo los datos y como estan en tipo de dato objeto, los convierto a string 
-    let cart = getFromLocalStorage(); 
-    cart.push(item); // tengo que llamar al local storage y pushear el item, guarda el objeto en la Ultima posicion 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(cart)); 
-} 
-
-export function setItemTolocalStorage(items){ // guardo los datos y como estan en tipo de dato objeto, los convierto a string 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(items)); 
-}
->>>>>>> 956ab6e495737521dd31f0e8e9272a262b46cdea
