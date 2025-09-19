32167859b333e74685de1b21181df79b039a94af
viviana <lilith37.vl@gmail.com>
2025-09-18 22:54:09 -0300

    actualizacion modal.js

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,19 +1,20 @@
+import { getFromLocalStorage, saveToLocalStorage, setItemTolocalStorage } from "./storage/storage.js"; 
+import { contador } from "./count.js"; 
+import { addEventListeners } from "./count.js"; 
+
+
 export function Modal(prod) {
   const container = document.querySelector("#prodModal");
 
   container.innerHTML = `
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
       <div class="modal-content">
         
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="prodModalLabel">
             ${prod.title} Modal title
           </h1>
-          <button 
-            type="button" 
-            class="btn-close" 
-            data-bs-dismiss="modal" 
-            aria-label="Close">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
           </button>
         </div>
 
@@ -21,15 +22,14 @@ export function Modal(prod) {
           <div class="row">
             
             <div class="col-md-6">
-              <img 
-                src="${prod.image}" 
-                class="img-fluid" 
-                alt="${prod.title}" 
-                style="height: 300px; width: 259px; object-fit: contain;">
+              <img src="${prod.image}" class="img-fluid" alt="${prod.title}" style="height: 300px; width: 259px; object-fit: contain;">
             </div>
 
-            <div class="col-md-6">
-              <p>${prod.description}</p>
+            <div class="col-md-6 d-flex justify-content-center align-items-center">
+              <div class="d-flex flex-column gap-3"> 
+                <p>${prod.description}</p>
+                <p style = "width: 150px;"> Price: USD $${prod.price}</p> 
+              </div>
             </div>
 
             <div class="col-12 d-flex justify-content-end align-items-start">
@@ -37,19 +37,17 @@ export function Modal(prod) {
                 Price: USD ${prod.price}
               </p>
             </div>
-
-            Aquí va el contenido dinámico que pongamos en el modal...
-          </div>
+          </div> 
+          ${contador(prod.id)} 
+            
         </div>
+        
 
         <div class="modal-footer">
-          <button 
-            type="button" 
-            class="btn btn-secondary" 
-            data-bs-dismiss="modal">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
             Cerrar
           </button>
-          <button type="button" class="btn btn-primary">
+          <button type="button" class="btn btn-primary" id="addToCartBtn-${prod.id}" data-id="${prod.id}"> 
             Agregar al Carrito
           </button>
         </div>
@@ -57,8 +55,21 @@ export function Modal(prod) {
       </div>
     </div>
   `;
-  const modalElement = document.getElementById("prodModal");
-  const bootstrapModal = new bootstrap.Modal(modalElement);
-  bootstrapModal.show();
+
+  const bootstrapModal = new bootstrap.Modal(container); 
+  bootstrapModal.show(); 
+  addEventListeners(prod.id, 1); 
+  const btnAddToCart = document.querySelector(`#addToCartBtn-${prod.id}`); 
+  btnAddToCart.addEventListener("click", () => { 
+    prod.qqty = ++document.querySelector(`#Contador-${prod.id}`).textContent; 
+    // valido si existe en el localStorage, mediante el metodo creado en Storage.js 
+    let dataStorage = getFromLocalStorage(); 
+    let filtered = dataStorage.filter(p => p.id !== prod.id); 
+    filtered.push(prod); 
+    setItemTolocalStorage(filtered); 
+  // saveToLocalStorage(prod); 
+  }) 
 }
+
+
 
